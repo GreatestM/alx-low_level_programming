3f5e6afca0b28189abc35cf8b717a75252a3c73d
GreatestM <86297375+GreatestM@users.noreply.github.com>
2021-09-06 20:53:00 +0200

    initial commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+* rev_string - Write a function that reverses a string.
+* @s: a variable string as referencia
+*
+* Return: void
+*/
+
+void rev_string(char *s)
+{
+int i = 0, z = 0;
+char a, ze;
+/*char nully = '\0';*/
+
+while (*(s + i))
+{
+i++;
+}
+z = i - 1;
+
+i = 0;
+
+for (; z > i; z--)
+{
+a = *(s + i);
+ze = *(s + z);
+*(s + i++) = ze;
+*(s + z) = a;
+}
+}

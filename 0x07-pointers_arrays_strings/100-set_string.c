ef1894ddb28dace6313393fe3e35c27a7499a0a3
GreatestM <86297375+GreatestM@users.noreply.github.com>
2021-09-13 18:07:57 +0200

    initial commit

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,15 +1,2 @@
-#include "holberton.h"
-
-/**
-* set_string - sets the value of a pointer to a char
-* @s: double pointer
-* @to: pointer
-*
-* Return : Void function .
-*/
-void set_string(char **s, char *to)
-{
-*s = to;
-}
 
 
